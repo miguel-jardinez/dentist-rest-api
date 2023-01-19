704fe3c0c2b45574e8ad0686adf2cc7214779ffc
miguel_jardinez <jardinez.ramos.miguel@gmail.com>
2023-01-18 20:34:46 -0600

    fix: trying to deploy nestjs in vercel

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT');
-  const VERSION = configService.get<string>('API_VERSION');
-  app.setGlobalPrefix(VERSION);
+  const PORT = configService.get<number>('PORT') || '3000';
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Common Dentist rest API')
     .setDescription('Api with dentist services')
-    .setVersion(VERSION)
+    .setVersion('1.0')
     .addTag('Dentist API')
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup(`${VERSION}/api`, app, document);
+  SwaggerModule.setup('/api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
